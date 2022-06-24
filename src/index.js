65ddc748111289528dd26937906a365ac53d25c9
s024 <s024@s024.net>
2022-06-24 14:10:06 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+/**
+ * アロー関数
+ */
+
+// 従来の関数
+function func1(str) {
+  return str;
+}
+console.log(func1("func1です"));
+
+// 従来の関数2
+const func2 = function (str) {
+  return str;
+};
+console.log(func2("func2です"));
